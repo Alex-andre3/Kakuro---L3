3f8426ed6e64ecf085abe34ef22172834c291e5f
Antoine-M2 <79085886+Antoine-M2@users.noreply.github.com>
2021-05-31 11:05:08 +0200

    Create script_dictionary.py

---
+++ src/solver/script_dictionary.py
@@ -0,0 +1,27 @@
+def creer_dictionnaire():
+    d = {}
+    l = []
+
+    for i in range(1, 2**9):
+        addition = []
+        nb_de_chiffres = 0
+        total_chiffres = 0
+        chiffre_i = 1
+        for c in bin(i)[:1:-1]:
+            if c == '1':
+                addition.append(chiffre_i)
+                nb_de_chiffres += 1
+                total_chiffres += chiffre_i
+            chiffre_i += 1
+        if nb_de_chiffres >= 2:
+            if total_chiffres in d:
+                if nb_de_chiffres in d[total_chiffres]:
+                    d[total_chiffres][nb_de_chiffres].append(addition)
+                else:
+                    d[total_chiffres][nb_de_chiffres] = [addition]
+            else:
+                d[total_chiffres] = {nb_de_chiffres: [addition]}
+                
+            #print(addition)
+
+    return d
